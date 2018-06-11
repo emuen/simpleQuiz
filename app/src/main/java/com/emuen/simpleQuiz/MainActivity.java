0afa0af1429d083cda7b31943474fadc9bd9665b
Morten <mail@mortenrasmussen.net>
2018-06-11 22:54:17 +0200

    Europas lande tilføjet

--- app/src/main/java/com/emuen/simpleQuiz/MainActivity.java
+++ app/src/main/java/com/emuen/simpleQuiz/MainActivity.java
@@ -1,26 +1,16 @@
 package com.emuen.simpleQuiz;
 
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MenuItem;
 import android.widget.TextView;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String question;
+        String answerCorrect;
+        String answerWrong1;
+        String answerWrong2;
+        String answerWrong3;
+
+        Random r = new Random();
+
         try {
             // get JSONObject from JSON file
             JSONObject obj = new JSONObject(loadJSONFromResources());
@@ -40,6 +38,38 @@ public class MainActivity extends AppCompatActivity {
             JSONArray userArray = obj.getJSONArray("questions");
             Log.d("Simple", "userArray " + userArray.length() );
             // implement for loop for getting users list data
+
+            int i1 = r.nextInt(userArray.length());
+            int i2 = r.nextInt(userArray.length());
+            int i3 = r.nextInt(userArray.length());
+            int i4 = r.nextInt(userArray.length());
+            while (i1 == i2) {
+                i2 = r.nextInt(userArray.length());
+            }
+            while ((i3 == i1) || (i3 == i2)) {
+                i3 = r.nextInt(userArray.length());
+            }
+            while ((i4 == i1) || (i4 == i2) || (i4 == i3)) {
+                i4 = r.nextInt(userArray.length());
+            }
+
+            JSONObject userDetail = userArray.getJSONObject(i1);
+            question = userDetail.getString("question");
+            answerCorrect = userDetail.getString("answer");
+            userDetail = userArray.getJSONObject(i2);
+            answerWrong1 = userDetail.getString("answer");
+            userDetail = userArray.getJSONObject(i3);
+            answerWrong2 = userDetail.getString("answer");
+            userDetail = userArray.getJSONObject(i4);
+            answerWrong3 = userDetail.getString("answer");
+
+            Log.d("Simple", "Spørgsmål: " + question);
+            Log.d("Simple", "Svar korrekt: " + answerCorrect);
+            Log.d("Simple", "Svar forkert 1: " + answerWrong1);
+            Log.d("Simple", "Svar forkert 2: " + answerWrong2);
+            Log.d("Simple", "Svar forkert 3: " + answerWrong3);
+
+            /*
             for (int i = 0; i < userArray.length(); i++) {
                 // create a JSONObject for fetching single user data
                 JSONObject userDetail = userArray.getJSONObject(i);
@@ -48,14 +78,14 @@ public class MainActivity extends AppCompatActivity {
                 // fetch email and name and store it in arraylist
                 question.add(userDetail.getString("question"));
                 answer.add(userDetail.getString("answer"));
-
             }
+                */
         } catch (JSONException e) {
             Log.d("Simple", "JSONEXCEPTION: " + e);
             e.printStackTrace();
         }
 
-        Log.d("Simple", "onCreate " + question );
+        //Log.d("Simple", "onCreate " + question );
 
         mTextMessage = (TextView) findViewById(R.id.message);
 
